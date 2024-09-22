15cf8c4946e70bce32c7950df53cb31f8f0b3697
Kevin Hudgens <hudgens1073@gmail.com>
2024-09-22 15:20:32 -0500

    cleaned up on how methods are being called

--- src/scraper.py
+++ src/scraper.py
@@ -2,16 +2,45 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-def activeDriver():
-    
-   driver = webdriver.Chrome()
-   bAScrape(driver)
-   driver.close()
 
-def bAScrape(driver):
-    driver.get("https://careers.boozallen.com/jobs/search/?1485=Japan&listFilterMode=1&jobRecordsPerPage=20&")
-    print(driver.title)
-    
-def getTitles(driver):
-    titles = driver.find_elements_by_class_name("cell-title")
-    
+class Scraper:
+
+    def __init__(self):
+        self.driver = webdriver.Safari()
+        self.companiesToSearch = {
+            1: "https://careers.boozallen.com/jobs/search/?1485=Japan&listFilterMode=1&jobRecordsPerPage=20&",
+            2: "https://www.lockheedmartinjobs.com/search-jobs/Japan",
+            3: "https://www.northropgrumman.com/jobs?_job_search=Japan",
+        }
+
+    def companySelector(self):
+        for key, url in self.companiesToSearch.items():
+            print(key)
+            self.getReqTitles(key, url)
+
+    def getReqTitles(self, key, url):
+        match key:
+            case 1:
+                self.driver.get(url)
+                reqs = self.driver.find_elements(By.CLASS_NAME, "cell-title")
+                for i in reqs:
+                    print(i.text)
+            case 2:
+                self.driver.get(url)
+                reqs = self.driver.find_elements(By.CLASS_NAME, "job-title")
+                for i in reqs:
+                    print(i.text)
+                    # ! CASE 3 still needs work
+            case 3:
+                self.driver.get(url)
+                self.driver.implicitly_wait(2)
+                reqs = self.driver.find_elements(
+                    By.CLASS_NAME,
+                    "col-sm-9",
+                )
+
+                for i in reqs:
+                    print(i.find_element(By.TAG_NAME, "h2").text)
+
+    def closeDriver(self):
+        self.driver.close()
