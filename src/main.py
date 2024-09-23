e6e063b7aba67e23a3fa5aacf4d101ab126fc2ec
Kevin Hudgens <hudgens1073@gmail.com>
2024-09-22 22:04:13 -0500

    changed vars to snake case

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
 from scraper import Scraper
+from data import FormatData
 
 
 def main():
     scraper = Scraper()
-    scraper.companySelector()
+    scraper.company_selector()
+    scraper.close_driver()
+
+    dataFormatter = FormatData(scraper.unstructured_data, {})
+    dataFormatter.prep_data()
+    dataFormatter.create_results_file()
 
 
 if __name__ == "__main__":
